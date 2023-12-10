ba46d5f2d03494df8104f1bbdc4e7adb16456a81
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-09 20:53:20 -0500

    started navbar

---
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from './Navbar.module.css';
+
+export const Navbar = () => {
+    return <nav className={styles.navbar}>
+        <a className={styles.title} href="/">Portfolio</a>
+        <div className={styles.menu}>
+            <ul className={styles.menuItems}>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>;
+}
