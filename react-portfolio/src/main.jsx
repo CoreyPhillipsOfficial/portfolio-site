d369d8683490de2bef299a9e505f4370ef9d39bf
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-09 19:27:17 -0500

    deleted css

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import '@fontsource/outfit'
+import '@fontsource/roboto'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
