8061467b27c731ccb282b67ab97b55a9d941464f
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-11 16:14:23 -0500

    imported skills json file

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
+import skills from '../../data/skills.json';
+import { getImageUrl } from '../../utils';
+
 export const Experience = () => {
-    return
+    return (
+        <section id='experience'>
+            <h2>Experience</h2>
+            <div>
+                <div>{
+                    skills.map((skill, id) => {
+                        return <div key={id}>
+                            <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
+                            <p>{skill.title}</p>
+                        </div>
+                    })
+                }</div>
+                <ul></ul>
+            </div>
+        </section>
+    );
 }
