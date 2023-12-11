aa5ef58d029a1cd8e86a8a2f0cd76a8bb5b94220
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-11 14:21:00 -0500

    started About section

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+
+import styles from './About.module.css';
 import { getImageUrl } from '../../utils';
 
 export const About = () => {
     return (
-        <section>
-            <h2>About</h2>
-            <div>
-                <img src={getImageUrl('about/aboutImage.png')} alt="Me sitting with a laptop"
+        <section className={styles.container}>
+            <h2 className={styles.title}>About</h2>
+            <div className={StyleSheet.content}>
+                <img
+                    src={getImageUrl('about/aboutImage.png')}
+                    alt="Me sitting with a laptop"
+                    className={styles.aboutImage}
                 />
-                <ul>
-                    <li><img src={getImageUrl('about/cursonIcon.png')} alt="Cursor icon" /></li>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl('about/cursorIcon.png')} alt="Cursor icon" />
+                        <div className='styles.aboutItemText'>
+                            <h3>Frontend Developer</h3>
+                            <p>
+                                I am a frontend developer with experience in building responsive and optimized websites.
+                            </p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl('about/serverIcon.png')} alt="Server icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend Developer</h3>
+                            <p>
+                                I have experience developing fast and optimized backend systems and APIs.
+                            </p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl('about/serverIcon.png')} alt="UI icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>UI Designer</h3>
+                            <p>
+                                I have designed multiple landing pages and have created design systems as well.
+                            </p>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </section>
