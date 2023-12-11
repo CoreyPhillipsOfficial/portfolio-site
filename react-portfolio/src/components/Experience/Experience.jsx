440c2acca68a7d1d04941a6f2b1bfdf5e332a692
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-11 16:46:36 -0500

    continued work on Experience

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 
+import styles from './Experience.module.css';
 import skills from '../../data/skills.json';
 import { getImageUrl } from '../../utils';
 
 export const Experience = () => {
     return (
-        <section id='experience'>
-            <h2>Experience</h2>
-            <div>
-                <div>{
-                    skills.map((skill, id) => {
-                        return <div key={id}>
-                            <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
-                            <p>{skill.title}</p>
-                        </div>
-                    })
-                }</div>
-                <ul></ul>
+        <section id='experience' className={styles.container}>
+            <h2 className={styles.title}>Experience</h2>
+            <div className={styles.content}>
+                <div >
+                    {skills.map((skill, id) => {
+                        return (
+                            <div key={id} className={styles.skill}>
+                                <div className={styles.skillImageContainer}>
+                                    <img
+                                        src={getImageUrl(skill.imageSrc)}
+                                        alt={skill.title} />
+                                </div>
+                                <p>{skill.title}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </section>
     );
