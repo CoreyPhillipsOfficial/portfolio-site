4031f12832b004382de50d2dafd8b4c86a627223
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-12 14:50:02 -0500

    Contact links open in new tab

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -16,11 +16,11 @@ export const Contact = () => {
             </li>
             <li className={styles.link}>
                 <img src={getImageUrl('contact/linkedinIcon.png')} alt="LinkedIn icon" />
-                <a href="https://www.linkedin.com/in/coreyphillipslinked/">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/coreyphillipslinked/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </li>
             <li className={styles.link}>
                 <img src={getImageUrl('contact/githubIcon.png')} alt="Github icon" />
-                <a href="https://github.com/CoreyPhillipsOfficial">Github</a>
+                <a href="https://github.com/CoreyPhillipsOfficial" target="_blank" rel="noopener noreferrer">Github</a>
             </li>
         </ul>
     </footer>;
