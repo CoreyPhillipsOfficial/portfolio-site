2c7b4b4833235233b63f37cd677b3567003d6382
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-11 21:45:42 -0500

    working on Projects

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import styles from './Projects.module.css';
+import styles from "./Projects.module.css";
 
-import projects from '../../data/projects.json';
+import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
     return (
-        <section className={styles.container}>
-            <h2>Projects</h2>
-            <div>
+        <section className={styles.container} id="projects">
+            <h2 className={styles.title}>Projects</h2>
+            <div className={styles.projects}>
                 {projects.map((project, id) => {
                     return <ProjectCard key={id} project={project} />;
                 })}
