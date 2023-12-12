c3c9a03877c3e78957a24781f9c76cafcd68464b
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-12 13:57:15 -0500

    Contact section complete

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -1,22 +1,24 @@
 import React from "react";
+
+import styles from './Contact.module.css';
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
-    return <footer>
-        <div>
+    return <footer id="contact" className={styles.container}>
+        <div className={styles.text}>
             <h2>Contact</h2>
             <p>Feel free to reach out!</p>
         </div>
-        <ul>
-            <li>
+        <ul className={styles.links}>
+            <li className={styles.link}>
                 <img src={getImageUrl('contact/emailIcon.png')} alt="Email icon" />
                 <a href="mailto:coreytphillips@icloud.com">Email</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl('contact/linkedinIcon.png')} alt="LinkedIn icon" />
                 <a href="https://www.linkedin.com/in/coreyphillipslinked/">LinkedIn</a>
             </li>
-            <li>
+            <li className={styles.link}>
                 <img src={getImageUrl('contact/githubIcon.png')} alt="Github icon" />
                 <a href="https://github.com/CoreyPhillipsOfficial">Github</a>
             </li>
