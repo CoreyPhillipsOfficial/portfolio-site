0c90361c1b95fed2b459add47c6f9625646d853d
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-11 20:53:40 -0500

    projects showing on page

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
+import styles from './Projects.module.css';
+
 import projects from '../../data/projects.json';
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
-    return <section>
-        <h2>Projects</h2>
-        <div>
-            {
-                projects.map((project, id) => {
-                    return
-                })
-            }
-        </div>
-    </section>;
-}
+    return (
+        <section className={styles.container}>
+            <h2>Projects</h2>
+            <div>
+                {projects.map((project, id) => {
+                    return <ProjectCard key={id} project={project} />;
+                })}
+            </div>
+        </section>
+    );
+};
