8b191e5b3ead657ed694a8fcfaf1f396beed06cc
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-12-23 14:02:25 -0500

    made links open in new tab

--- react-portfolio/src/components/Projects/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -25,13 +25,13 @@ export const ProjectCard = ({
                 })}
             </ul>
             <div className={styles.links}>
-                <a href={demo} className={styles.link}>
+                <a href={demo} className={styles.link} target='_blank' rel='noopener noreferrer'>
                     Demo
                 </a>
-                <a href={source} className={styles.link}>
+                <a href={source} className={styles.link} target='_blank' rel='noopener noreferrer'>
                     Source
                 </a>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
