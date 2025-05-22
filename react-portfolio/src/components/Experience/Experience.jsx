cba56a8de4dd707692316603a4071537f6d718c3
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-05-21 22:41:11 -0400

    Added newest project

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -8,7 +8,7 @@ import { getImageUrl } from '../../utils';
 export const Experience = () => {
     return (
         <section id='experience' className={styles.container}>
-            <h2 className={styles.title}>Experience</h2>
+            <h2 className={styles.title}>Skills</h2>
             <div className={styles.content}>
                 <div className={styles.skills}>
                     {skills.map((skill, id) => {
